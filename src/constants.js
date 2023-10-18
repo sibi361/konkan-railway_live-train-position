884d0a74071fdcd6b4a4470ebf563aa4366a465d
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-10-19 00:37:27 +0530

    Fix Github Link On Frontend

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const env = {
+    DEBUG: true,
+    REPO_URL: "https://github.com/sibi361/konkan-railway_live-train-position",
+};
+
+export default env;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/styles.css";
-import env from "../api/_constants.js";
+import env from "./constants.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
