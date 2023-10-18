76a0843209a7939dd1d52510f0bacaa1b1ea546d
sibi361 <104310887+sibi361@users.noreply.github.com>
2023-10-18 14:10:05 +0530

    Update Playwright Args

--- api/_constants.js
+++ api/_constants.js
@@ -4,7 +4,20 @@ export default {
     UPSTREAM_URL: "https://konkanrailway.com/VisualTrain/",
     REPO_URL: "https://github.com/sibi361/konkan-railway_live-train-position",
     API_VERSION: 3,
-    PLAYWRIGHT_OPTS: { headless: true },
+    PLAYWRIGHT_OPTS: {
+        headless: true,
+        args: [
+            "--disable-extensions",
+            "--disable-background-networking",
+            "--disable-default-apps",
+            "--disable-gpu",
+            "--disable-sync",
+            "--disable-translate",
+            "--no-first-run",
+            "--incognito",
+            "--safebrowsing-disable-auto-update",
+        ],
+    },
     PLAYWRIGHT_DEVICE: "Desktop Firefox",
     SERVER_ERROR_MESSAGE: "Server overloaded. Please wait.",
 };
