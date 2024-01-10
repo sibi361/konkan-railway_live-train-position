2769988836cbb32bf71e8324f2cba3d91af5942d
sibi361 <104310887+sibi361@users.noreply.github.com>
2024-01-10 19:07:15 +0530

    Update Status Page URL

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const env = {
     DEBUG: true,
     REPO_URL: "https://github.com/sibi361/konkan-railway_live-train-position",
-    STATUS_PAGE_URL: "https://stats.uptimerobot.com/G2DNMTWOmk/795501650",
+    STATUS_PAGE_URL: "https://stats.uptimerobot.com/lmzW2uOmpK/796149885",
 };
 
 export default env;
